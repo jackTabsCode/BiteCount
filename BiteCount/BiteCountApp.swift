97ef9f58a41a844ae1198ee6efa225711d763682
Jack Taylor <44332148+jackTabsCode@users.noreply.github.com>
2023-06-23 17:44:39 -0700

    Initial Commit

---
+++ BiteCount/BiteCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteCountApp.swift
+//  BiteCount
+//
+//  Created by Jack Taylor on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BiteCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
